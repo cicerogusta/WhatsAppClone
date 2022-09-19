1eb13fa327a9e25b5c675f3073dd81ff3d6d3d66
cicero <cicero.pinheiro@dock.tech>
2022-09-19 16:14:21 -0300

    Alterado tipo do parâmetro para fragmento

--- app/src/main/java/com/ciceropinheiro/whatsapp_clone/adapter/TabViewPagerAdapter.kt
+++ app/src/main/java/com/ciceropinheiro/whatsapp_clone/adapter/TabViewPagerAdapter.kt
@@ -1,15 +1,13 @@
 package com.ciceropinheiro.whatsapp_clone.adapter
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ciceropinheiro.whatsapp_clone.R
 import com.ciceropinheiro.whatsapp_clone.ui.fragments.chamadas.ChamadasFragment
 import com.ciceropinheiro.whatsapp_clone.ui.fragments.conversas.ConversasFragment
-import com.ciceropinheiro.whatsapp_clone.ui.fragments.home.HomeFragment
 import com.ciceropinheiro.whatsapp_clone.ui.fragments.status.StatusFragment
 
-class TabViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+class TabViewPagerAdapter(fa: Fragment) : FragmentStateAdapter(fa) {
     val tabs = arrayOf(R.string.conversas, R.string.status, R.string.chamadas)
     val fragments = arrayOf(ConversasFragment(),StatusFragment(), ChamadasFragment())
     override fun getItemCount(): Int = fragments.size
